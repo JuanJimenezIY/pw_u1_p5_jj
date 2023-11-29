2ba91e7e325851e3523a4cdeec5a6a63aa06fce3
Juan Jimenez <sr.j_599@hotmail.com>
2023-11-29 17:14:54 -0500

    tarea2

--- js/calculadoraFuncionesDeber.js
+++ js/calculadoraFuncionesDeber.js
@@ -1,9 +1,11 @@
 function reset() {
     document.getElementById('idResultado').innerText = '';
 }
-
+let num1;
 function operacion(op) {
-    document.getElementById('idResultado').innerText =  document.getElementById('idResultado').innerText+op;
+    num1=document.getElementById('idResultado').innerText =  document.getElementById('idResultado').innerText+op;
+    document.getElementById('idResultado').innerText = '';
+   
 }
 
 function numeros(num) {
@@ -12,7 +14,9 @@ function numeros(num) {
 }
 
 function resultado() {
-    let respuesta = document.getElementById('idResultado').innerText;
+
+    let num2 = document.getElementById('idResultado').innerText;
+    let respuesta=num1+num2;
     let res = eval(respuesta);
     document.getElementById('idResultado').innerText = res;
 }
